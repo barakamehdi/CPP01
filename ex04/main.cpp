b514b8d613e4714b81ea91dbf461df8077c7fe05
barakamehdi <barakamehdi3@gmail.com>
2025-03-22 01:52:39 +0000

    working on ex04

---
+++ ex04/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+std :: string ft_replace( std::string text, std::string search , std::string replace){
+
+    for (int i = 0 ; i < text.length() ;){
+        
+    }
+}
+int main(int argc, char**argv){
+
+    std :: string filename;
+
+    std :: string SearCh;
+
+    std :: string RepLace;
+
+
+    if (argc != 3){
+
+        std :: cout << "error : INVALID OR MISSING INPUT !!" << std::endl;
+        exit (0);    
+    }
+
+    filename = argv[1];
+    SearCh = argv[2];
+    RepLace = argv[3];
+    
+
+
+
+    
+
+}
